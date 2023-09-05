7838f947deab1ed665f8b23aedd3e32fe9bfbb19
Camilo <dev1@e-advising.biz>
2023-09-04 23:14:04 -0500

    Pr

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,14 @@
+
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+
+export class MainPageComponent  {
+
+
+}
